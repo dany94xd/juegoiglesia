454ea620f8e6a2a4f05b52a3e66bc624ce93e128
sebastianmite <62480872+sebastianmite@users.noreply.github.com>
2021-03-19 18:47:41 -0500

    Marzo 19 2021
    v1.0

---
+++ TempleRunIglesia/Assets/Scripts/Player.cs
@@ -0,0 +1,210 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float laneSpeed;
+    public float jumpLength;
+    public float jumpHeight;
+    public float slideLength;
+    private Rigidbody rb;
+    private Animator anim;
+    private BoxCollider boxCollider;
+    private int currentLane = 1;
+    private Vector3 verticalTargetPosition;
+    private bool jumping = false;
+    private float jumpStart;
+    private bool sliding = false;
+    private float slideStart;
+    private Vector3 boxColliderSize;
+    private bool isSwipping = false;
+    private Vector2 startingTouch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
+        boxCollider = GetComponent<BoxCollider>();
+        boxColliderSize = boxCollider.size;
+        anim.Play("runStart");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            ChangeLane(-1);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow)){
+            ChangeLane(1);
+        }else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Jump();
+        }else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Slide();
+        }
+        if (Input.touchCount == 1)
+        {
+            if (isSwipping)
+            {
+                Vector2 diff = Input.GetTouch(0).position - startingTouch;
+                diff = new Vector2(diff.x / Screen.width, diff.y / Screen.width);
+                if (diff.magnitude > 0.01f)
+                {
+                    if (Mathf.Abs(diff.y) > Mathf.Abs(diff.x))
+                    {
+                        if (diff.y < 0)
+                        {
+                            Slide();
+                        }
+                        else
+                        {
+                            Jump();
+                        }
+                    }
+                    else
+                    {
+                        if (diff.x < 0)
+                        {
+                            ChangeLane(-1);
+                        }
+                        else
+                        {
+                            ChangeLane(1);
+                        }
+                    }
+
+                    isSwipping = false;
+                }
+            }
+
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                startingTouch = Input.GetTouch(0).position;
+                isSwipping = true;
+            }
+            else if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                isSwipping = false;
+            }
+        }
+
+
+
+        if (jumping)
+        {
+            float ratio = (transform.position.z - jumpStart) / jumpLength;
+            if (ratio >= 1f)
+            {
+                jumping = false;
+                anim.SetBool("Jumping", false);
+            }
+            else
+            {
+                verticalTargetPosition.y = Mathf.Sin(ratio * Mathf.PI) * jumpHeight;
+            }
+        }
+        else
+        {
+            verticalTargetPosition.y = Mathf.MoveTowards(verticalTargetPosition.y, 0, 5 * Time.deltaTime);
+        }
+
+        if (sliding)
+        {
+            float ratio = (transform.position.z - slideStart) / slideLength;
+            if (ratio >= 1f)
+            {
+                sliding = false;
+                anim.SetBool("Sliding", false);
+                boxCollider.size = boxColliderSize;
+            }
+        }
+        Vector3 targetPosition = new Vector3(verticalTargetPosition.x, verticalTargetPosition.y, transform.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, laneSpeed* Time.deltaTime  );
+
+
+
+        if(Input.touchCount == 1)
+        {
+            if (isSwipping)
+            {
+                Vector2 diff = Input.GetTouch(0).position - startingTouch;
+                diff = new Vector2(diff.x / Screen.width, diff.y / Screen.width);
+                if(diff.magnitude > 0.01f)
+                {
+                    if (Mathf.Abs(diff.y) > Mathf.Abs(diff.x))
+                    {
+
+                    }
+                    else
+                    {
+                        if (diff.x < 0)
+                        {
+                            ChangeLane(-1);
+                        }
+                        else
+                        {
+                            ChangeLane(1);
+                    
+                        }
+
+                    }
+                    isSwipping = false;
+
+                }
+            }
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                startingTouch = Input.GetTouch(0).position;
+                isSwipping = true;
+
+            }
+            else if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                isSwipping = false;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = Vector3.forward * speed;
+    }
+    void ChangeLane(int direction)
+    {
+        int targetLane = currentLane + direction;
+        if (targetLane < 0 || targetLane > 2)
+            return;
+        currentLane = targetLane;
+        verticalTargetPosition = new Vector3((currentLane - 1), 0, 0);
+    }
+    void Jump()
+    {
+        if (!jumping)
+        {
+            jumpStart = transform.position.z;
+            anim.SetFloat("JumpSpeed", speed / jumpLength);
+            anim.SetBool("Jumping", true);
+            jumping = true;
+        }
+    }
+
+    void Slide()
+    {
+        if (!jumping && !sliding)
+        {
+            slideStart = transform.position.z;
+            anim.SetFloat("JumpSpeed", speed / slideLength);
+            anim.SetBool("Sliding", true);
+            Vector3 newSize = boxCollider.size;
+            newSize.y = newSize.y / 2;
+            boxCollider.size = newSize;
+            sliding = true;
+
+        }
+    }
+}
